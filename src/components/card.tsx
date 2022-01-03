74e0bec081cd13bb539d541bc571c74fcb0796d8
mcst <marcelstuewe@gmail.com>
2022-01-03 21:15:36 +0100

    open links to files

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -19,17 +19,18 @@ interface ICard{
 }
 export const Card:FC<ICard> = (props) =>{
     const {realEstate} = props;
-    const {titel, grundbuch, termin, verkehrswert, lastModTime, googleMaps, exposee, gutachten, foto} = realEstate||{};
+    const {titel, grundbuch, termin, verkehrswert, lastModTime, googleMaps, exposee, gutachten, foto, aktenzeichen} = realEstate||{};
     console.log(titel)
     const card = <div className={"card"}>
         <div className="title">{titel}</div>
         <div className="termin">{termin}</div>
+        <div className="aktenzeichen"><label>Aktenzeichen:</label> {aktenzeichen}</div>
         <div className="verkehrswert">{verkehrswert}</div>
         <div className="lastModTime">{lastModTime}</div>
         <div><a href={googleMaps} target="_blank">google maps</a></div>
-        <div><a href={`localhost:3000/zvglink${gutachten}`} target="_blank">gutachten</a></div>
-        <div><a href={`localhost:3000/zvglink${exposee}`} target="_blank">exposee</a></div>
-        <div><a href={`localhost:3000/zvglink${foto}`} target="_blank">foto</a></div>
+        {gutachten ? <div><a href={`http://localhost:3000/zvglink?${gutachten}`}>gutachten</a></div> : null}
+        {exposee ? <div><a href={`http://localhost:3000/zvglink?${exposee}`}>exposee</a></div> : null}
+        {foto? <div><a href={`http://localhost:3000/zvglink?${foto}`}>foto</a></div> : null}
     </div>
     return card;
 }
